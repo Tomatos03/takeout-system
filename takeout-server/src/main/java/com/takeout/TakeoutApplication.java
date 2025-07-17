f9720c07b9483019e836a2f11acf74b9cb632686
Tomatos <3333331969@qq.com>
2025-07-17 21:55:23 +0800

    feat: init project

---
+++ takeout-server/src/main/java/com/takeout/TakeoutApplication.java
@@ -0,0 +1,15 @@
+package com.takeout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : Tomatos
+ * @date : 2025/7/17
+ */
+@SpringBootApplication
+public class TakeoutApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TakeoutApplication.class, args);
+    }
+}
